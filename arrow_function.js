b1c029d646c6bccc26652c1eb35c338f663239bf
unknown <aadityaruhela810@gmail.com>
2026-09-03 11:38:33 +0530

    pattern

--- arrow_function.js
+++ arrow_function.js
@@ -8,3 +8,17 @@ hello();
 
 hello = () => console.log("hello world");
 hello();
+
+
+// pattern
+const pattern = (n) => {
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+
+        for (let j = 1; j <= i; j++) {
+            row += "* ";
+        }
+        console.log(row);
+    }
+};
+pattern(5);
