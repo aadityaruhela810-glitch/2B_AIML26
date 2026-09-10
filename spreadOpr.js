6d71471325b3d3dfa3a266c8cf703d4778571336
unknown <aadityaruhela810@gmail.com>
2026-09-10 10:44:26 +0530

    spread

--- spreadOpr.js
+++ spreadOpr.js
@@ -1,9 +1,41 @@
-function sum(...value){
-    let sum = 0;
-    for(let i in value){
-        sum = sum+value[i];
+// function sum(...value){
+//     let sum = 0;
+//     for(let i in value){
+//         sum = sum+value[i];
+//     }
+//     console.log(sum);
+// }
+// let arr = [10,20,30,40];
+// sum(...arr);    
+
+
+//object literal
+
+// let name="Aadi";
+// let rollNo=1234;
+// let data = {
+//     name,
+//     rollNo
+// };
+// console.log(data)
+
+// // 
+
+// let f="firstname";
+// let l="lastname";
+//     let data1={
+//         [f]:"Aadi",
+//         [l]:"rajput"
+//     }
+//     console.log(data1.firstname,data1.lastname);
+
+let f="firstname";
+let l="lastname";
+    let data={
+        [f]:"Aadi",
+        [l]:"rajput"
     }
-    console.log(sum);
-}
-let arr = [10,20,30,40];
-sum(...arr);
+    "detail show"(){
+        console.log(this.first)
+        console.log(this.last)
+    }
